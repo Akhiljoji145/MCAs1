af1012b022c46545ae13cbf8c48fb24cfdfb614e
Akhiljoji145 <akhiljoji1451@gmail.com>
2024-11-26 22:54:23 -0800

    modified

--- python/factorial2.py
+++ python/factorial2.py
@@ -1,11 +1,11 @@
 from math import factorial
-a=int(input("enter a number"))
-print(f"factorial of a is :",factorial(a))
+c=int(input("enter a number"))
+print(f"factorial of a is :",factorial(c))
 num=3
-c,b=0,1
+a,b=0,1
 for _ in range(num):
-    print(c,end=" ")
-    c,b=b,c+b
+    print(a,end=" ")
+    a,b=b,a+b
 list1=[1,2,3,4,5]
 sum1=sum(list1)
 print("\n",sum1)
