136c3b67a1bf555fc21df763a9d2139d48f2132c
Akhiljoji145 <akhiljoji1451@gmail.com>
2024-12-10 20:07:37 -0800

    modified

---
+++ webdevelopment/sort_array.php
@@ -0,0 +1,10 @@
+<?php
+$a=array("Anjala","chirstenna","blessey","angela");
+print_r($a);
+echo"<br>Ascending Order : ";
+asort($a);
+print_r($a);
+echo"<br>Descending order : ";
+arsort($a);
+print_r($a);
+?>
